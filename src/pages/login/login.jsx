32f67a6adbb8ba3e9f9ffce8e5b81d6ea762a4da
yuanquan <2993298917@qq.com>
2021-12-04 23:31:27 +0800

    feat: 引入 material-ui

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { TextField, Button } from '@material-ui/core'
 
 function Login() {
   return (
-    <div>
-      Login
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      width: 300
+    }}>
+      <TextField label="Outlined secondary" color="secondary" focused />
+      <Button>
+        Login
+      </Button>
     </div>
   );
 }
