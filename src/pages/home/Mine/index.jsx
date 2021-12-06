fa8b04800ce0187706cf5b450b8bf59afdc38f73
yuanquan <2993298917@qq.com>
2021-12-06 19:25:26 +0800

    添加用户头像

--- src/pages/home/Mine/index.jsx
+++ src/pages/home/Mine/index.jsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 
 export default function Mine() {
   return (
-    <Box>
-      233333
-      <h2>nihao</h2>
+    <Box
+    sx={{ position: 'absolute', top: 40 ,left: '40%',}}>
+      <Stack>
+        <Avatar/>用户名
+      </Stack>
     </Box>
   );
 }
