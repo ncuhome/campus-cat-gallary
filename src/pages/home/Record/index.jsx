e1405caf14dd13936cb93e65074d8af580834004
kii <andy864940012@gmail.com>
2021-12-06 23:27:08 +0800

    new waterfall style

--- src/pages/home/Record/index.jsx
+++ src/pages/home/Record/index.jsx
@@ -1,125 +1,48 @@
-import React from "react";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import styled from "styled-components";
+import { WaterfallGrid } from "react-waterfall-grid";
+import { useMediaQuery } from "react-responsive";
 
-const itemData = [
-  {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-    author: "@bkristastucchio",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-    author: "@rollelflex_graphy726",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-    author: "@helloimnik",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-    author: "@nolanissac",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    author: "@hjrc33",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-  },
-];
+// You are free to add as many grid contents as you want. Here, you see only 4 pictures.
+import Image1 from "../../../image/hack.png";
+import Image2 from "./2.png";
+import Image3 from "./3.png";
+import Image4 from "./4.png";
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+const ParentContainer = styled.div`
+  width: 100%;
+  background-color: #121212;
+`;
 
-function RecipeReviewCard() {
-  const [expanded, setExpanded] = React.useState(false);
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+const Image = styled.img`
+  object-fit: cover;
+`;
+
+export default function App() {
+  // Boolean - True if phone
+  const isPhone = useMediaQuery({ query: "max-width: 480px" });
+
+  // List of images <img>
+  const imagesList = [Image1, Image2, Image3, Image4].map((imagePath) => (
+    <Image
+      key={imagePath}
+      style={{ width: isPhone ? "200px" : "300px" }}
+      src={imagePath}
+      alt={imagePath}
+    />
+  ));
 
   return (
-    <Card sx={{ maxWidth: 345, margin: "auto", marginTop: 2 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="title"
-        subheader="September 14, 2016"
+    <ParentContainer id="parent-container">
+      <WaterfallGrid
+        children={imagesList}
+        childWidth={isPhone ? 200 : 300}
+        styleGridContainer={{
+          width: "100%",
+          position: "relative",
+          justifyContent: "center",
+          zIndex: 1,
+        }}
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>Heat 1/2 cup of the brot</Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    </ParentContainer>
   );
-}
-
-export default function Record() {
-  return <RecipeReviewCard></RecipeReviewCard>;
 }
